0cb6442505ae19522b6d0e666ded5b9b3d715901
David <davidk@mrcodeswildride.com>
2018-11-21 15:54:22 +0000

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+// Section 1: Make a single variable for all of the squares.
+// You can name it anything you want, but since it represents multiple squares, the name should be plural, like squares.
+
+
+// Section 2: Make a variable for the button.
+// You can name it anything you want, but for simplicity, name it the same as the ID.
+
+
+// Section 3: Add separate event listeners to the squares and button.
+// You can name the functions anything you want, but pick something that indicates action.
+
+
+// Section 4: Make separate functions to toggle a square and clear the grid.
